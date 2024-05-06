810a962a25131352161e1930dfd3b39b31039b00
notbutter17 <lipaj7687@gmail.com>
2024-05-06 11:42:59 -0500

    Login

---
+++ SysAlmacenVC/src/main/java/pe/edu/upeu/syscenterlife/modelo/MenuMenuItenTO.java
@@ -0,0 +1,17 @@
+package pe.edu.upeu.syscenterlife.modelo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class MenuMenuItenTO {
+
+    public String menunombre, menuitemnombre, nombreObj;
+
+
+}
